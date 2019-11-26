d4ce8f18e56c5229f07b5282f6cf706fd95f2a8d
Teo Marcelo <tmfiles@Teos-MacBook-Pro.local>
2019-11-25 23:50:42 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,69 @@
-console.log("Up and running!");
 
-var cards = ["queen", "king", "queen", "king"];
+
+var cards = [ 
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
+
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push("cardOne");
-console.log("User flipped " + cardOne);
+var checkForMatch = function () {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
+};
 
-var cardTwo = cards [3];
-cardsInPlay.push("cardTwo");
-console.log("User flipped " + cardTwo);
+var flipcard = function () {
+    var cardId = this.getAttribute('data-id');
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 
-if (cardsInPlay[0] === cardsInPlay[3]) {
-    alert("You found a match!);
-} else {
-    alert("Sorry, try again.");
-}
+   
+    this.setAttribute('src', cards[cardId].cardImage);
+
+    if (cardsInPlay[0] === cardsInPlay[2]) {
+        alert("You found a match!");
+        checkForMatch();
+    } else {
+        alert("Sorry, try again.");
+    }
+    
+};
+
+var createBoard = function () {
+    for (i=0, i < cards.length; i++);
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    button.addEventListener('click', flipCard);
+    document.getElementById(game-board).appendChild(cardElement)
+};
+
+createBoard();
+
 
 
 // console.log("User flipped " + cardThree);
+
